88551e1adbf25e7a3be9cf10d72d62e053aa9849
Julio Martinez <julio.martinez@kukenantech.com>
2022-07-19 22:02:03 -0400

    create component c-helper-item

---
+++ src/components/c-helper-item.vue
@@ -0,0 +1,30 @@
+<template>
+	<div class="c-helper-item">
+		<div>titulo</div>
+		<div></div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'c-helper-item',
+
+	props: {
+		title: String,
+		text: String,
+	},
+};
+</script>
+
+<style lang="scss" scoped>
+.c-helper-item {
+	width: 330px;
+	height: 80px;
+	border-top: solid 1px RGB(var(--color-primary-light));
+	border-bottom: solid 1px RGB(var(--color-primary-light));
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	margin-top: 20px;
+}
+</style>
--- src/views/v-helper.vue
+++ src/views/v-helper.vue
@@ -1,16 +1,34 @@
 <template>
 	<l-page>
-		<div class="v-helper__title" slot="header">
-			<c-icon class="v-helper__title-icon" src="@icons/customerService" />
-			<div>
-				<div>
+		<div class="v-helper__header" slot="header">
+			<c-icon class="v-helper__header-icon" src="@icons/customerService" />
+			<div class="v-helper__text-group">
+				<div class="v-helper__user-text">
 					{{ this.$t('HELLO') + ' ' + userName }}
 				</div>
-				<div>
+				<div class="v-helper__title-text">
 					{{ this.$t('HELPER.TITLE') }}
 				</div>
 			</div>
 		</div>
+		<section class="v-helper__section">
+			<h2 class="v-helper__section-title">
+				{{ this.$t('HELPER.TITLE') }}
+			</h2>
+			<div class="v-helper__items">
+				<c-helper-item />
+			</div>
+		</section>
+		<section class="v-helper__section">
+			<h2 class="v-helper__section-title">
+				{{ this.$t('HELPER.SEARCH') }}
+			</h2>
+			<c-text-field class="v-helper__input-search">
+				<c-text-field-icon>
+					<c-icon></c-icon>
+				</c-text-field-icon>
+			</c-text-field>
+		</section>
 	</l-page>
 </template>
 
@@ -18,10 +36,13 @@
 import { mapState } from 'vuex';
 import lPage from '@layouts/l-page';
 import CIcon from '@components/c-icon';
+import CTextField from '@components/c-text-field';
+import CTextFieldIcon from '@components/c-text-field-icon';
+import CHelperItem from '@components/c-helper-item';
 
 export default {
 	name: 'v-helper',
-	components: { lPage, CIcon },
+	components: { lPage, CIcon, CTextField, CTextFieldIcon, CHelperItem },
 
 	computed: {
 		...mapState('session', ['userName']),
@@ -30,7 +51,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.v-helper__title {
+.v-helper__header {
 	display: flex;
 	justify-content: center;
 	align-items: center;
@@ -38,7 +59,21 @@ export default {
 	gap: 15px;
 	padding-top: 40px;
 }
-.v-helper__title-icon {
+.v-helper__header-icon {
 	font-size: 26px;
+}
+.v-helper__section {
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: flex-start;
+	padding: 60px 60px 10px 60px;
+}
+.v-helper__section-title {
+	font-size: 2rem;
+	font-weight: 500;
+}
+.v-helper__input-search {
+	margin-top: 20px;
 }
 </style>
